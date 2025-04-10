9538098710a942dceea30556d52a02acbc73b113
Atharv Sheersh Chaturvedi <144057375+AtharvSChaturvedi@users.noreply.github.com>
2025-04-10 20:36:00 +0530

    Update Q5.java

--- javalab2/Q5.java
+++ javalab2/Q5.java
@@ -1,23 +1,29 @@
-import java.util.Arrays;
-import java.util.HasMap;
+import java.util.*;
 
-public class Q5{
-	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Enter the number of elements: ");
-		int size=scanner.nextInt();
-		
-		int [] num=new int[size];
-		
-		System.out.println("Enter "+size+" numbers: ");
-		for(int i=0;i<size;i++){
-			System.out.print("Enter Number"+(i+1)+": ");
-			num[i]=scanner.nextInt();
-		}
-		
-		HashMap<Integer, Integer> o
-		System.out.println(" ");
-		scanner.close();
-	}
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter list of numbers (separated by spaces): ");
+        String inputLine = scanner.nextLine();
+        
+        String[] tokens = inputLine.trim().split("\\s+");
+        List<Integer> numbers = new ArrayList<>();
+        for (String token : tokens) {
+            numbers.add(Integer.parseInt(token));
+        }
+
+        Map<Integer, Integer> frequencyMap = new LinkedHashMap<>();
+
+        for (int num : numbers) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        System.out.println("\nOccurrences:");
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            System.out.println("Occurrence of " + entry.getKey() + " = " + entry.getValue());
+        }
+
+        scanner.close();
+    }
 }
